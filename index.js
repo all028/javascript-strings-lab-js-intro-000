dad5b96b9be5be61d5cba64d9b622b345786107d
al <albliu@email.com>
2019-11-16 02:46:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-const topic = 'space';
+const topic = `space`;
 
 const conversation = `${topic}`;
